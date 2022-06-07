bf41503a459ff14baf217a1077102bade341ec66
Md Sajidur rahman <sajid.ict@gmail.com>
2022-06-07 07:09:14 +0600

    print error logs from file

--- batch.py
+++ batch.py
@@ -1,3 +1,5 @@
+from datetime import date
+import linecache
 class Batch:
   def __init__(self, name, age):
     self.name = name
@@ -7,7 +9,11 @@ class Batch:
     print("Hello my name is " + self.name)
 
   def ProcessLog(self):
-    print("Hello my name is " + self.name)
-
+    fileDate = str(date.today())
+    theBody = []
+    tmpFile = open(r'C:\My data\Guru Project\python-batch-log-process\samplelog.log')
+    for line in tmpFile:
+        if 'Error' in line: print(line )    
+    tmpFile.close() 
 p1 = Batch("John", 36)
-p1.sendMail()
+p1.ProcessLog()
